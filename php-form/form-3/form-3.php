6604a9d793b5c3c497fc4c4e78a6e84224e483a0
Muhammad Shanewaz <2016-3-60-050@std.ewubd.edu>
2021-04-05 16:41:06 +0600

    3rd commit

---
+++ php-form/form-3/form-3.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <?php
+
+    ?>
+
+    <h2>Registration Form</h2>
+
+    <form action="action_php.php" method="post">
+        <label for="name">Name:</label>
+        <input type="text" name="fname" id="" placeholder="enter first name">
+        <input type="text" name="lname" id="" placeholder="enter last name">
+
+
+
+        <br><br>
+        <button type="reset" class="resetbtn">RESET</button>
+        <button type="submit" class="submitbtn">SUBMIT</button>
+    </form>
+
+
+</body>
+</html>
